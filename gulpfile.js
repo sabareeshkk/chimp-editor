7b8bb45600afdac5f4c7c4f9b19da400d091ecb7
sabareeshk <sabareeshk1991@gmail.com>
2017-07-18 19:09:03 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const babel = require('rollup-plugin-babel')
+const cssnano = require('gulp-cssnano')
+const del = require('del')
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const Rollup = require('rollup')
+const rollup = require('gulp-rollup')
+const run = require('run-sequence')
+const sass = require('gulp-sass')
+const size = require('gulp-size')
+const uglify = require('rollup-plugin-uglify')
+const inject = require('rollup-plugin-inject')
+
+gulp.task('clean', () => del(['./dist']))
+
+const rollupConfig = minimize => ({
+  rollup: Rollup,
+  entry: './src/pell.js',
+  moduleName: 'pell',
+  format: 'umd',
+  exports: 'named',
+  plugins: [babel({ exclude: 'node_modules/**'}), inject({exclude: 'node_modules/**', $: 'jquery'})].concat(
+    minimize
+      ? [
+        uglify({
+          compress: { warnings: false },
+          mangle: true,
+          sourceMap: false
+        })
+      ]
+      : []
+  )
+})
+
+gulp.task('script', () => {
+  gulp.src('./src/*.js')
+    .pipe(rollup(rollupConfig(false)))
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest('./dist'))
+
+  gulp.src('./src/*.js')
+    .pipe(rollup(rollupConfig(true)))
+    .pipe(rename('pell.min.js'))
+    .pipe(size({ showFiles: true }))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('style', () => {
+  gulp.src(['./src/pell.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
+    .pipe(cssnano())
+    .pipe(rename('pell.min.css'))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('default', ['clean'], () => {
+  run('script', 'style')
+  gulp.watch('./src/pell.scss', ['style'])
+  gulp.watch('./src/pell.js', ['script'])
+})
---
+++ src/pell.js
@@ -0,0 +1,200 @@
+import $ from "jquery";
+
+console.log('$', $);
+
+const actions = {
+  bold: {
+    icon: '<b>B</b>',
+    title: 'Bold',
+    result: () => exec('bold')
+  },
+  italic: {
+    icon: '<i>I</i>',
+    title: 'Italic',
+    result: () => exec('italic')
+  },
+  underline: {
+    icon: '<u>U</u>',
+    title: 'Underline',
+    result: () => exec('underline')
+  },
+  strikethrough: {
+    icon: '<strike>S</strike>',
+    title: 'Strike-through',
+    result: () => exec('strikeThrough')
+  },
+  heading1: {
+    icon: '<b>H<sub>1</sub></b>',
+    title: 'Heading 1',
+    result: () => exec('formatBlock', '<H1>')
+  },
+  heading2: {
+    icon: '<b>H<sub>2</sub></b>',
+    title: 'Heading 2',
+    result: () => exec('formatBlock', '<H2>')
+  },
+  paragraph: {
+    icon: '&#182;',
+    title: 'Paragraph',
+    result: () => {
+      exec('formatBlock', '<P>')
+      // removeDivs()
+    }
+  },
+  quote: {
+    icon: '&#8220; &#8221;',
+    title: 'Quote',
+    result: () => exec('formatBlock', '<BLOCKQUOTE>')
+  },
+  olist: {
+    icon: '&#35;',
+    title: 'Ordered List',
+    result: () => exec('insertOrderedList')
+  },
+  ulist: {
+    icon: '&#8226;',
+    title: 'Unordered List',
+    result: () => exec('insertUnorderedList')
+  },
+  code: {
+    icon: '&lt;/&gt;',
+    title: 'Code',
+    result: () => exec('formatBlock', '<PRE>')
+  },
+  line: {
+    icon: '&#8213;',
+    title: 'Horizontal Line',
+    result: () => exec('insertHorizontalRule')
+  },
+  link: {
+    icon: '&#128279;',
+    title: 'Link',
+    result: () => {
+      const url = window.prompt('Enter the link URL')
+      if (url) exec('createLink', url)
+    }
+  },
+  image: {
+    icon: '&#128247;',
+    title: 'Image',
+    result: () => {
+      // console.log('hello world');
+      // const url = window.prompt('Enter the image URL')
+      const modalString = `<div id="myModal" class="modal">
+                              <div class="modal-content">
+                                  <span class="close">&times;</span>
+                                  <p>Some text in the Modal..</p>
+                                  <input type="text" name="enter" class="enter" value="" id="imageUrl"/>
+                                  <input type="button" value="click" id="imageButton"/>
+                              </div>
+                          </div>`
+      InitializeModal(modalString)
+      // if (url) exec('insertImage', url)
+    }
+  }
+}
+
+const InitializeModal = function(modalString){
+
+      const modal = document.createElement('figure')
+      $(modal).attr('id', 'modalFigure');
+      modal.innerHTML = modalString
+      document.body.appendChild(modal);
+      var myModal = document.getElementById('myModal');
+      myModal.style.display = "block";
+      var span = document.getElementsByClassName("close")[0];
+      span.onclick = function() {
+          modal.style.display = "none"; 
+          $("#modalFigure").remove(); 
+      }
+      const modalSubmit = $('#imageButton');
+      modalSubmit.click(() => {
+            const modalVal = $('#imageUrl')         
+            const url = modalVal.val()
+            console.log('url', url);
+            if (url) {
+              modal.style.display = "none";
+              $("#modalFigure").remove(); 
+              $('.pell-content').focus();
+              exec('insertImage', url)
+            }
+      });
+}
+
+const classes = {
+  actionbar: 'pell-actionbar',
+  button: 'pell-button',
+  content: 'pell-content'
+}
+
+export const exec = (command, value = null) => {
+  document.execCommand(command, false, value);
+  observeImages();
+}
+
+const observeImages = () => {
+     $('.pell-content img').each(function (i, s) {
+       console.log('sevent', i, s);
+                $(s).off( "click");
+                $(s).click((event) => {
+                    console.log('helloo you clicked the img', this, event);
+                    $(s).attr('id', 'img'+i);
+                });
+            });
+}
+
+const preventTab = event => {
+  if (event.which === 9) event.preventDefault()
+  else if (event.which === 13){
+    console.log('enter key');
+    document.execCommand('insertHTML', false, '<br><br>');
+    event.preventDefault();
+  }
+}
+
+export const init = settings => {
+  settings.actions = settings.actions
+    ? settings.actions.map(action => {
+      if (typeof action === 'string') return actions[action]
+      else if (actions[action.name]) return { ...actions[action.name], ...action }
+      return action
+    })
+    : Object.keys(actions).map(action => actions[action])
+  
+  settings.classes = { ...classes, ...settings.classes }
+  
+  const actionbar = document.createElement('div')
+  actionbar.className = settings.classes.actionbar
+  settings.element.appendChild(actionbar)
+
+  createContentEditable(settings);
+  
+  createButtons(actionbar, settings.actions, settings);
+
+  if (settings.styleWithCSS) exec('styleWithCSS')
+
+  return settings.element
+}
+
+const createContentEditable = settings => {
+  settings.element.content = document.createElement('div')
+  settings.element.content.contentEditable = true
+  settings.element.content.className = settings.classes.content
+  settings.element.content.oninput = event => settings.onChange(event.target.innerHTML)
+  settings.element.content.onkeydown = preventTab
+  settings.element.appendChild(settings.element.content)
+}
+
+const createButtons = (actionbar, buttons, settings) => {
+    buttons.forEach(action => {
+    const button = document.createElement('button')
+    button.className = settings.classes.button
+    button.innerHTML = action.icon
+    button.title = action.title
+    button.onclick = action.result
+    actionbar.appendChild(button)
+  })
+}
+
+
+export default { exec, init }
